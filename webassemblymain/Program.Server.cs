0ee9681ee6335834f99189a65e50458428177726
jacalvar <jacalvar@microsoft.com>
2023-05-17 21:28:47 +0200

    All templates created

---
+++ webassemblymain/Program.Server.cs
@@ -0,0 +1,36 @@
+
+namespace webassemblymain;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddWebassemblyComponents();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.MapRazorComponents<App>();
+
+        app.Run();
+    }
+}
